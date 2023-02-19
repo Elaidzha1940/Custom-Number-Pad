c566825c542b2370bbb0dc64b62a0b3dd9d39578
Elaidzha1940 <eli080293@gmail.com>
2023-02-20 00:24:01 +0800

    Code

---
+++ Custom Number Pad/Custom Number Pad/Custom_Number_PadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_Number_PadApp.swift
+//  Custom Number Pad
+//
+//  Created by Elaidzha Shchukin on 20.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Custom_Number_PadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
